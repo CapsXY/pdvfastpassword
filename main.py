4b5b7819b17dc3080a2e12e8cfbfcb614410293e
Caio Souza <31600751+CapsXY@users.noreply.github.com>
2025-09-05 17:30:00 -0300

    Added new timezones and adjusted application completion.

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ def convert_number(num_str: str) -> str:
     return "".join([table[int(ch)] for ch in num_str])
 
 
+def generate_etiqueta(dt: datetime.datetime) -> str:
+    mes = dt.strftime("%m")
+    dia = dt.strftime("%d")
+    hora = dt.strftime("%H")
+    return convert_number(mes) + convert_number(dia) + convert_number(hora)
+
+
 def generate_syncplus(dt: datetime.datetime) -> str:
     ano = dt.strftime("%y")
     mes = dt.strftime("%m")
@@ -28,11 +35,11 @@ def generate_syncplus(dt: datetime.datetime) -> str:
     return convert_number(ano) + convert_number(mes) + convert_number(dia) + convert_number(hora)
 
 
-def generate_etiqueta(dt: datetime.datetime) -> str:
-    mes = dt.strftime("%m")
-    dia = dt.strftime("%d")
-    hora = dt.strftime("%H")
-    return convert_number(mes) + convert_number(dia) + convert_number(hora)
+def now_with_offset(gmt_offset: int) -> datetime.datetime:
+    """Retorna datetime ajustado para o fuso solicitado"""
+    # Brasil: UTC-3 é o padrão
+    utc_now = datetime.datetime.utcnow()
+    return utc_now + datetime.timedelta(hours=gmt_offset)
 
 
 class App:
@@ -63,26 +70,33 @@ class App:
             pos = ((64 - text_w) // 2, (64 - text_h) // 2)
             d.text(pos, text, font=font, fill="white")
 
-        def copy_syncplus(icon, item):
-            password = generate_syncplus(datetime.datetime.now())
-            pyperclip.copy(password)
-
         def copy_etiqueta(icon, item):
             password = generate_etiqueta(datetime.datetime.now())
+            pyperclip.copy(password)
+
+        def copy_syncplus_gmt3(icon, item):
+            password = generate_syncplus(now_with_offset(-3))
+            pyperclip.copy(password)
+
+        def copy_syncplus_gmt4(icon, item):
+            password = generate_syncplus(now_with_offset(-4))
             pyperclip.copy(password)
 
         def quit_app(icon, item):
             icon.stop()
             self.root.quit()
+            os._exit(0)
 
         menu = (
-            pystray.MenuItem("Sync Plus", copy_syncplus),
             pystray.MenuItem("Etiqueta", copy_etiqueta),
+            pystray.MenuItem("Sync Plus (GMT-3)", copy_syncplus_gmt3),
+            pystray.MenuItem("Sync Plus (GMT-4)", copy_syncplus_gmt4),
             pystray.MenuItem("Sair", quit_app),
         )
 
-        icon = pystray.Icon("senha_app", image, "Gerador de Senha", menu)
+        icon = pystray.Icon("senha_app", image, "Gerador de Senhas", menu)
         icon.run()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
